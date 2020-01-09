70d5eec6a81674131f2a9658d05145c53cf1bbc0
Prasanna Balaji <erprasanna97@gmail.com>
2020-01-09 16:10:53 +0530

    Updates

---
+++ precache-manifest.c1f986241eeed9137cc0ffd61d8fd0fd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d3a27dbcc1fa75814b28dbd95de5eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e0eacb93553c71191f0",
+    "url": "/static/css/2.372bada3.chunk.css"
+  },
+  {
+    "revision": "8acc68e68019890acfb7",
+    "url": "/static/css/main.b7e470ec.chunk.css"
+  },
+  {
+    "revision": "8e0eacb93553c71191f0",
+    "url": "/static/js/2.3109f545.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.3109f545.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8acc68e68019890acfb7",
+    "url": "/static/js/main.ae0464af.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "f7fbf898752c0db050e78c4065f13774",
+    "url": "/static/media/Arrange-Puzzle-Game.f7fbf898.png"
+  },
+  {
+    "revision": "3496a5d311a1943de970e8f8a3d0a361",
+    "url": "/static/media/Dev-Picture.3496a5d3.jpg"
+  },
+  {
+    "revision": "b0c981509242577a588e32539a2c2d6d",
+    "url": "/static/media/EmployeesProfileApp.b0c98150.png"
+  },
+  {
+    "revision": "9001a857d6a1fc9731f7e13fd4a6abda",
+    "url": "/static/media/Multi-Dropdown.9001a857.png"
+  },
+  {
+    "revision": "03a02e720a402ecc083a4a78541090b5",
+    "url": "/static/media/NobelPrizeIcon.03a02e72.jpeg"
+  },
+  {
+    "revision": "2067de1164aa6b663ca247b93a23e824",
+    "url": "/static/media/ProfilePicture.2067de11.png"
+  },
+  {
+    "revision": "fb3a7e74d15c49e92716b0e7ddf3f2a6",
+    "url": "/static/media/library.fb3a7e74.jpg"
+  },
+  {
+    "revision": "59c1df9f2f3b3860325b59b0d7189996",
+    "url": "/static/media/nodeJS-api.59c1df9f.png"
+  }
+]);
