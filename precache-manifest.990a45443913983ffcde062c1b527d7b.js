022de0fa1edf28870a063170fd08be9004125fbe
Prasanna Balaji <erprasanna97@gmail.com>
2020-01-09 16:55:24 +0530

    Updates

---
+++ precache-manifest.990a45443913983ffcde062c1b527d7b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04727d614cbdd2b124a65628c3ce53b1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8e0eacb93553c71191f0",
+    "url": "/Portfolio/static/css/2.372bada3.chunk.css"
+  },
+  {
+    "revision": "b80b763978abfa6a68bf",
+    "url": "/Portfolio/static/css/main.b7e470ec.chunk.css"
+  },
+  {
+    "revision": "8e0eacb93553c71191f0",
+    "url": "/Portfolio/static/js/2.3109f545.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Portfolio/static/js/2.3109f545.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b80b763978abfa6a68bf",
+    "url": "/Portfolio/static/js/main.787496f5.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  },
+  {
+    "revision": "f7fbf898752c0db050e78c4065f13774",
+    "url": "/Portfolio/static/media/Arrange-Puzzle-Game.f7fbf898.png"
+  },
+  {
+    "revision": "3496a5d311a1943de970e8f8a3d0a361",
+    "url": "/Portfolio/static/media/Dev-Picture.3496a5d3.jpg"
+  },
+  {
+    "revision": "b0c981509242577a588e32539a2c2d6d",
+    "url": "/Portfolio/static/media/EmployeesProfileApp.b0c98150.png"
+  },
+  {
+    "revision": "9001a857d6a1fc9731f7e13fd4a6abda",
+    "url": "/Portfolio/static/media/Multi-Dropdown.9001a857.png"
+  },
+  {
+    "revision": "03a02e720a402ecc083a4a78541090b5",
+    "url": "/Portfolio/static/media/NobelPrizeIcon.03a02e72.jpeg"
+  },
+  {
+    "revision": "2067de1164aa6b663ca247b93a23e824",
+    "url": "/Portfolio/static/media/ProfilePicture.2067de11.png"
+  },
+  {
+    "revision": "fb3a7e74d15c49e92716b0e7ddf3f2a6",
+    "url": "/Portfolio/static/media/library.fb3a7e74.jpg"
+  },
+  {
+    "revision": "59c1df9f2f3b3860325b59b0d7189996",
+    "url": "/Portfolio/static/media/nodeJS-api.59c1df9f.png"
+  }
+]);
